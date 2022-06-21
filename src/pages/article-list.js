d1d0bdbae8e1adfafdf9e8c3bb8438d5f9f2c0f4
GarryLaly <garrylaly@gmail.com>
2022-06-21 15:41:38 +0700

    Integration axios

--- src/pages/article-list.js
+++ src/pages/article-list.js
@@ -1,60 +1,89 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
 
 const ArticleList = () => {
-  const [listData, setListData] = useState([
-    {
-      title: "Article 1",
-      slug: "article-1",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Article 2",
-      slug: "article-2",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Article 3",
-      slug: "article-3",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Article 4",
-      slug: "article-4",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Article 5",
-      slug: "article-5",
-      content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-  ]);
+  const user = "1";
+  const [listData, setListData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(
+        "http://hp-api.herokuapp.com/api/characters"
+      );
+
+      setListData(response.data.filter((item, index) => index < 10));
+      setIsLoading(false);
+    } catch (e) {
+      setIsLoading(false);
+      console.error("error", e);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div style={{ padding: 20 }}>
-      {listData.map((item) => (
-        <Link
-          to={`/article/detail/${item.slug}`}
-          style={{ color: "black", textDecoration: "none" }}
-        >
-          <div
+    <div style={{ padding: "2%" }}>
+      {user === null && <Navigate to="/auth" replace={true} />}
+      {isLoading && <div>Loading...</div>}
+      <div
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          display: "flex",
+          maxWidth: 500,
+        }}
+      >
+        {listData.map((item) => (
+          <Link
+            to={`/article/detail/${item.slug}`}
             style={{
-              background: "#eee",
-              marginBottom: 20,
-              padding: 10,
-              borderRadius: 10,
+              color: "black",
+              textDecoration: "none",
+              width: "48%",
+              margin: "1%",
             }}
           >
-            <div>{item.title}</div>
-            <div>{item.content.substring(0, 50) + "..."}</div>
-          </div>
-        </Link>
-      ))}
+            <div
+              style={{
+                color: "black",
+                textDecoration: "none",
+                backgroundColor: "#ccc",
+                marginBottom: 5,
+                borderRadius: 10,
+                padding: 10,
+              }}
+            >
+              <img
+                src={item.image}
+                style={{ width: "100%", height: "auto" }}
+                alt="Alternative"
+              />
+              <div
+                style={{
+                  fontSize: 18,
+                  marginTop: 5,
+                  fontWeight: "bold",
+                  color: "#000",
+                }}
+              >
+                {item.name}
+              </div>
+              <div style={{ color: "#333" }}>
+                {item.dateOfBirth === ""
+                  ? item.yearOfBirth === ""
+                    ? "Unknown"
+                    : item.yearOfBirth
+                  : item.dateOfBirth}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,7 +1,72 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState("Empty");
+
+  const actionLogin = async () => {
+    try {
+      setIsLoading(true);
+      // untuk form-data dan x-www-form-urlencoded
+      const formData = new FormData();
+      formData.append("username", username);
+      formData.append("password", password);
+
+      const response = await axios.post(
+        "http://localhost:8080/backend/login",
+        formData
+      );
+
+      // ini untuk raw json
+      // const response = await axios.post(
+      //   "http://localhost:8080/backend/login",
+      //   {
+      //     username,
+      //     password
+      //   }
+      // );
+
+      setResult(response.data);
+    } catch (e) {
+      setResult(e.response.data);
+      setIsLoading(false);
+      console.error("error", e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <label>Username</label>
+        <br />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <br />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={actionLogin}>Login</button>
+      </div>
+
+      <br />
+      <br />
+      <h5>Result:</h5>
+      <div>{JSON.stringify(result)}</div>
+    </div>
+  );
 };
 
 export default Auth;
